f40a83d38f294c344de90a0e9c9ef9f9c059a083
Muhammed-Jameel <76800652+Muhammed-Jameel@users.noreply.github.com>
2023-02-22 11:49:10 +0300

    Delete task1 directory

--- task1/src/assets/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Logo} from "./housen.svg"
-export { default as Changer} from './Vector.svg'
-export { default as MenuIcon} from './line.svg'
-export { default as Searchicon} from './Ellipse.svg'
-export { default as Arrow} from './Arrow.svg'
--- task1/src/pages/Empty.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Empty = () => {
-  return (
-    <div>Empty untill we get some content</div>
-  )
-}
-
-export default Empty
